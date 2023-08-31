f966e2980dc24730ac253510c27063ecf068740a
Dave Tang <davetingpongtang@gmail.com>
2023-08-31 17:00:32 +0900

    Update options

--- run_sarek.sh
+++ run_sarek.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 nextflow run nf-core/sarek \
+   -resume \
    -profile docker \
    --genome GATK.GRCh38 \
+   --wes true \
+   --intervals data/region/20130108.exome.targets.hg38.sorted.bed \
+   --tools haplotypecaller \
    --input samplesheet.csv \
    --outdir results
