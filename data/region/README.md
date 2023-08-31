5680d91e6a642cf5d129304279c3850965869526
Dave Tang <davetingpongtang@gmail.com>
2023-08-31 15:09:20 +0900

    Convert to hg38

---
+++ data/region/README.md
@@ -0,0 +1,7 @@
+# README
+
+Convert to hg38.
+
+```console
+./liftOver 20130108.exome.targets.bed hg19ToHg38.over.chain.gz 20130108.exome.targets.hg38.bed 20130108.exome.targets.hg38.unmapped.bed
+```
--- data/region/download.sh
+++ data/region/download.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 wget -c ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/exome_pull_down_targets/20130108.exome.targets.bed
+wget -c https://hgdownload.cse.ucsc.edu/goldenpath/hg19/liftOver/hg19ToHg38.over.chain.gz
