8e267ad97d6248ee38e56291efc619a16db65a80
Dave Tang <davetingpongtang@gmail.com>
2023-09-01 12:53:22 +0900

    Expected path

--- script/download_resource_bundle.sh
+++ script/download_resource_bundle.sh
@@ -6,8 +6,8 @@ script_dir=$(dirname $(realpath $0))
 
 genome=GRCh38
 
-if [[ ! -d ${script_dir}/../data/${genome} ]]; then
-   mkdir ${script_dir}/../data/${genome}
+if [[ ! -d ${script_dir}/../data/Homo_sapiens/GATK/${genome} ]]; then
+   mkdir -p ${script_dir}/../data/Homo_sapiens/GATK/${genome}
 fi
 
-aws s3 cp --recursive s3://ngi-igenomes/igenomes/Homo_sapiens/GATK/GRCh38 ${script_dir}/../data/${genome} --no-sign-request
+aws s3 cp --recursive s3://ngi-igenomes/igenomes/Homo_sapiens/GATK/GRCh38 ${script_dir}/../data/Homo_sapiens/GATK/${genome} --no-sign-request
