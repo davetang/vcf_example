e99fd8f417d73043774ce81169170a0d77a83fa8
Dave Tang <davetingpongtang@gmail.com>
2023-09-01 15:09:04 +0900

    Annotate variants

--- run_sarek.sh
+++ run_sarek.sh
@@ -3,9 +3,10 @@
 nextflow run nf-core/sarek \
    -resume \
    -profile docker \
+   --igenomes_base data \
    --genome GATK.GRCh38 \
    --wes true \
    --intervals data/region/20130108.exome.targets.hg38.sorted.bed \
-   --tools haplotypecaller \
+   --tools haplotypecaller,snpeff,vep \
    --input samplesheet.csv \
    --outdir results
