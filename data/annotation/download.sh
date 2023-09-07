e0cd963763add3a3f78cb710b6cb64f6ccce04d0
Dave Tang <davetingpongtang@gmail.com>
2023-09-07 10:49:24 +0900

    Download GTF

--- data/annotation/download.sh
+++ data/annotation/download.sh
@@ -2,3 +2,4 @@
 
 wget -c https://ftp.ensembl.org/pub/release-110/gff3/homo_sapiens/Homo_sapiens.GRCh38.110.chr.gff3.gz
 wget -c https://ftp.ensembl.org/pub/release-110/fasta/homo_sapiens/pep/Homo_sapiens.GRCh38.pep.all.fa.gz
+wget -c https://ftp.ensembl.org/pub/release-110/gtf/homo_sapiens/Homo_sapiens.GRCh38.110.chr_patch_hapl_scaff.gtf.gz
